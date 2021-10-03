fa8d810d14e7cded385b03a9b7251c8e7d8abc37
Yves Candau <ycandau@sfu.ca>
2021-10-02 23:30:40 -0700

    Strings: Comment Z value calculation

--- strings.js
+++ strings.js
@@ -11,13 +11,14 @@ const match = (str, pos1, pos2) => {
 // Z algorithm
 
 const getZValues = (str) => {
-  let pos = 0;
+  let pos = 1;
   let begin = -1;
   let end = -1;
-  let z = [];
-  let k = 0;
+  let z = [-1];
 
   while (pos < str.length) {
+    // Position beyond rightmost matching range
+    // Simple match
     if (pos > end) {
       const length = match(str, 0, pos);
       z[pos] = length;
@@ -26,15 +27,29 @@ const getZValues = (str) => {
         end = pos + length - 1;
       }
     } else if (pos <= end) {
+      // Position within range and Z value contained within range
+      // Z value is known and equal to value from prefix
       if (z[pos - begin] < end - pos + 1) {
         z[pos] = z[pos - begin];
-      } else {
+      }
+
+      // Position within range and Z value reaches beyond range
+      // Z value is at least equal to value from prefix
+      // and extend search beyond range
+      else {
         begin = pos;
         end += match(str, end - pos + 1, end + 1);
         z[pos] = end - pos + 1;
       }
     }
-
     pos++;
   }
+
+  return z;
+};
+
+//------------------------------------------------------------------------------
+
+module.exports = {
+  getZValues,
 };
