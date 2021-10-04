05ba8feef190dc4353eb1815d85bc4ce8bba2fc3
Yves Candau <ycandau@sfu.ca>
2021-10-03 23:24:09 -0700

    Searching: Add algorithm for binary search with rightmost match

--- searching.js
+++ searching.js
@@ -17,11 +17,30 @@ const binarySearchLeftmost = (array, value) => {
       right = mid;
     }
   }
-  return left;
+  return array[left] === value ? left : undefined;
+};
+
+//------------------------------------------------------------------------------
+// Binary search: Rightmost on multiple matches
+
+const binarySearchRightmost = (array, value) => {
+  let left = 0;
+  let right = array.length;
+
+  while (left < right) {
+    let mid = (left + right) >> 1;
+    if (array[mid] <= value) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+  return array[left - 1] === value ? left - 1 : undefined;
 };
 
 //------------------------------------------------------------------------------
 
 module.exports = {
   binarySearchLeftmost,
+  binarySearchRightmost,
 };
