64f1f1b9325cb49afb1425942a4f666074c3271f
Yves Candau <ycandau@sfu.ca>
2021-10-22 23:40:55 -0700

    Add breadth-first traversal

--- trees.js
+++ trees.js
@@ -8,3 +8,24 @@ class Node {
     this.children.push(...nodes);
   }
 }
+
+//------------------------------------------------------------------------------
+
+const traverse_BreadthFirst = (root, action) => {
+  const queue = [...root.children];
+
+  while (queue.length > 0) {
+    const node = queue.shift();
+    action(node);
+    queue.push(...node.children);
+  }
+};
+
+//------------------------------------------------------------------------------
+
+const traverse_DepthFirst = (root, action) => {
+  action(root);
+  for (const node of root.children) {
+    traverse_DepthFirst(node, action);
+  }
+};
